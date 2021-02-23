ba2fd00ca7db33689d38a7c438370f8ed3b89d5b
John Manack <jmnck28@gmail.com>
2021-02-23 16:05:47 -0500

    adds store and actions

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 'use strict';
 
+// Store
+const defaultState = {
+    checking: 100,
+    savings: 100
+};
+
+// Actions
+const createDeposit = (account, amount) => {
+    return {
+        type: 'deposit',
+        payload: {
+            account,
+            amount
+        }
+    }
+}
+
+const createWithdrawal = (account, amount) => {
+    return {
+        type: 'withdrawal',
+        payload: {
+            account,
+            amount
+        }
+    }
+}
